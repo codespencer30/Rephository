a97f03ee3de23001d0242ae10113071879cfc9aa
codespencer30 <147875771+codespencer30@users.noreply.github.com>
2023-10-14 01:48:48 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Calculate simple interest
+
+# Get the principal amount
+principal=$(echo "$1" | bc)
+
+# Get the time period
+time=$(echo "$2" | bc)
+
+# Get the rate of interest
+rate=$(echo "$3" | bc)
+
+# Calculate the simple interest
+si=$(echo "scale=2; ($principal * $time * $rate) / 100" | bc)
+
+# Print the simple interest
+echo "Simple interest: $si"
